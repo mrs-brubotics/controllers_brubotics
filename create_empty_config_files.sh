35be6aa4753fed58e279dad1a138ff5190b5d27d
Sofia Disegna <sofiacatharina@gmail.com>
2020-07-01 17:25:19 +0200

    Added script to automatically create config files

---
+++ create_empty_config_files.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Enter file name"
+read filename
+declare -a run_type_list=("simulation" "uav")
+declare -a uav_type_list=("eaglemk2" "f450" "f550" "m600" "t650" "eagle" "naki")
+
+cd ~/workspace/src/ros_packages_interns/controllers_brubotics/config
+
+for rtype in ${run_type_list[@]};do
+  mkdir -p $rtype
+  cd $rtype
+  for utype in ${uav_type_list[@]};do
+    mkdir -p $utype
+    cd $utype
+    touch $filename
+    cd ..
+  done
+  cd ..
+done
