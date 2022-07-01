c16e1311488d2e2a94676dc4987bcf19c94528a2
Bryan <bryan.convens@gmail.com>
2022-07-01 17:40:16 +0200

    refactored agains the new transformer (ctu backward incompatibility)

--- src/se3_copy_controller/se3_copy_controller.cpp
+++ src/se3_copy_controller/se3_copy_controller.cpp
@@ -647,7 +647,7 @@ const mrs_msgs::AttitudeCommand::ConstPtr Se3CopyController::update(const mrs_ms
     Ib_b_stamped.vector.y        = Ib_b_(1);
     Ib_b_stamped.vector.z        = 0;
 
-    auto res = common_handlers_->transformer->transformSingle(uav_state_.header.frame_id, Ib_b_stamped);
+    auto res = common_handlers_->transformer->transformSingle(Ib_b_stamped,uav_state_.header.frame_id);
 
     if (res) {
       Ib_w[0] = res.value().vector.x;
@@ -1028,7 +1028,7 @@ const mrs_msgs::AttitudeCommand::ConstPtr Se3CopyController::update(const mrs_ms
       Ep_stamped.vector.y        = Ep(1);
       Ep_stamped.vector.z        = Ep(2);
 
-      auto res = common_handlers_->transformer->transformSingle("fcu_untilted", Ep_stamped);
+      auto res = common_handlers_->transformer->transformSingle(Ep_stamped,"fcu_untilted");
 
       if (res) {
         Ep_fcu_untilted[0] = res.value().vector.x;
@@ -1048,7 +1048,7 @@ const mrs_msgs::AttitudeCommand::ConstPtr Se3CopyController::update(const mrs_ms
       Ev_stamped.vector.y        = Ev(1);
       Ev_stamped.vector.z        = Ev(2);
 
-      auto res = common_handlers_->transformer->transformSingle("fcu_untilted", Ev_stamped);
+      auto res = common_handlers_->transformer->transformSingle(Ev_stamped,"fcu_untilted");
 
       if (res) {
         Ev_fcu_untilted[0] = res.value().vector.x;
@@ -1165,7 +1165,7 @@ const mrs_msgs::AttitudeCommand::ConstPtr Se3CopyController::update(const mrs_ms
     world_accel.vector.y        = world_accel_y;
     world_accel.vector.z        = world_accel_z;
 
-    auto res = common_handlers_->transformer->transformSingle("fcu", world_accel);
+    auto res = common_handlers_->transformer->transformSingle(world_accel,"fcu");
 
     if (res) {
 
@@ -1317,7 +1317,7 @@ void Se3CopyController::switchOdometrySource(const mrs_msgs::UavState::ConstPtr&
   world_integrals.vector.y = Iw_w_[1];
   world_integrals.vector.z = 0;
 
-  auto res = common_handlers_->transformer->transformSingle(new_uav_state->header.frame_id, world_integrals);
+  auto res = common_handlers_->transformer->transformSingle(world_integrals,new_uav_state->header.frame_id);
 
   if (res) {
 
